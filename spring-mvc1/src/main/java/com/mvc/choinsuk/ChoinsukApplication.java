68ade1b7a9ee3948463f10a9214c0519967a9241
chionsuk <hsuk0409@gmail.com>
2021-12-03 20:46:53 +0900

    스프링 MVC 스터디 시작

---
+++ spring-mvc1/src/main/java/com/mvc/choinsuk/ChoinsukApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.choinsuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoinsukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoinsukApplication.class, args);
+	}
+
+}
---
+++ spring-mvc1/src/test/java/com/mvc/choinsuk/ChoinsukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.choinsuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoinsukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
