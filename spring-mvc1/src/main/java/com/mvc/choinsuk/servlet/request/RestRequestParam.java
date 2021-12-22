60e82766fbb3deafa59a01a29943caefd438a5dd
chionsuk <hsuk0409@gmail.com>
2021-12-22 22:49:53 +0900

    ResponseEntity 응답 처리

--- spring-mvc1/src/main/java/com/mvc/choinsuk/servlet/request/RestRequestParam.java
+++ spring-mvc1/src/main/java/com/mvc/choinsuk/servlet/request/RestRequestParam.java
@@ -1,13 +1,19 @@
 package com.mvc.choinsuk.servlet.request;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mvc.choinsuk.servlet.request.dto.BasicRequestBody;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/request")
+@RequiredArgsConstructor
 public class RestRequestParam {
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/param")
     public void service(@RequestParam(value = "username") String username,
@@ -18,11 +24,13 @@ public class RestRequestParam {
     }
 
     @PostMapping("/param")
-    public String service(@RequestBody BasicRequestBody requestBody) {
+    public ResponseEntity<String> service(@RequestBody BasicRequestBody requestBody) throws JsonProcessingException {
         System.out.println("requestBody = " + requestBody);
         System.out.println("username = " + requestBody.getUsername());
         System.out.println("age = " + requestBody.getAge());
 
-        return "200 OK";
+        String responseObj = objectMapper.writeValueAsString(requestBody);
+
+        return ResponseEntity.ok().body(responseObj);
     }
 }
